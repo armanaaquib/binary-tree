7c960229f325ed8a6b5f7d47dca8c147e0f4bb31
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-06-04 16:50:01 +0530

    Added: Js: remove

--- js/bst.js
+++ js/bst.js
@@ -28,4 +28,34 @@ const search = (root, searchKey) => {
   return search(root.right, searchKey);
 };
 
-module.exports = { insert, search };
+const remove = (root, value) => {
+  if (root == null) {
+    return root;
+  }
+
+  if (value < root.value) {
+    root.left = remove(root.left, value);
+  } else if (value > root.value) {
+    root.right = remove(root.right, value);
+  } else {
+    if (root.left == null) {
+      return root.right;
+    }
+
+    if (root.right == null) {
+      return root.left;
+    }
+
+    let minValueNode = root.right;
+    while (minValueNode.left != null) {
+      minValueNode = minValueNode.left;
+    }
+
+    root.value = minValueNode.value;
+    root.right = remove(root.right, minValueNode.value);
+  }
+
+  return root;
+};
+
+module.exports = { insert, search, remove };
--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { insert, search } = require('./bst');
+const { insert, search, remove } = require('./bst');
 
 describe('search()', function () {
   it('should return null value not in bst', function () {
@@ -7,8 +7,45 @@ describe('search()', function () {
     assert.strictEqual(search(root, -1), null);
   });
 
-  it('should return node value in bst', function () {
+  it('should return node in bst', function () {
     const root = [10, 20, 5, 8, 1, 15, 25].reduce(insert, null);
     assert.strictEqual(search(root, 5).value, 5);
+  });
+});
+
+describe('search()', function () {
+  it('should not delete node if value not in bst', function () {
+    let root = [10, 20, 5, 8, 1, 15, 25].reduce(insert, null);
+    root = remove(root, -1);
+    const exp_root = [10, 20, 5, 8, 1, 15, 25].reduce(insert, null);
+    assert.deepStrictEqual(root, exp_root);
+  });
+
+  it('should delete leaf node in bst', function () {
+    let root = [10, 20, 5, 8, 1, 15, 25].reduce(insert, null);
+    root = remove(root, 8);
+    const exp_root = [10, 20, 5, 1, 15, 25].reduce(insert, null);
+    assert.deepStrictEqual(root, exp_root);
+  });
+
+  it('should delete n - 1 depth node in bst', function () {
+    let root = [10, 20, 5, 8, 1, 15, 25].reduce(insert, null);
+    root = remove(root, 5);
+    const exp_root = [10, 20, 8, 1, 15, 25].reduce(insert, null);
+    assert.deepStrictEqual(root, exp_root);
+  });
+
+  it('should delete root depth node in bst', function () {
+    let root = [10, 20, 5, 8, 1, 15, 25].reduce(insert, null);
+    root = remove(root, 10);
+    const exp_root = [15, 20, 5, 8, 1, 25].reduce(insert, null);
+    assert.deepStrictEqual(root, exp_root);
+  });
+
+  it('should delete single child node in bst', function () {
+    let root = [10, 20, 5, 8, 1, 25].reduce(insert, null);
+    root = remove(root, 20);
+    const exp_root = [10, 25, 5, 8, 1].reduce(insert, null);
+    assert.deepStrictEqual(root, exp_root);
   });
 });
